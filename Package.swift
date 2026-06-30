5905c7537d2bb66f49e1a749a01f1aca3fd88fd4
siddharth <siddharth.k@mapmyindia.com>
2026-06-30 13:11:23 +0530

    bug fix and improvements

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MapplsLMS",
-            url: "https://mmi-api-team.s3.amazonaws.com/mappls-sdk-ios/mappls-lms/MapplsLMS.xcframework-2.0.3.zip",
+            url: "https://mmi-api-team.s3.amazonaws.com/mappls-sdk-ios/mappls-lms/MapplsLMS.xcframework-2.0.2.zip",
             checksum: "ff376e583553fc7388430f01be49bd40aeed79bdbbfca2ada994a09b004d6b9d"
         ),
         .target(
