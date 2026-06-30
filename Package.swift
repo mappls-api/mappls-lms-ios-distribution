2b4dd2567c5ab6b5a2fae0c24c30a6a661ac0ce1
siddharth <siddharth.k@mapmyindia.com>
2026-06-30 13:06:44 +0530

    improvements and bug fixes

--- Package.swift
+++ Package.swift
@@ -12,13 +12,13 @@ let package = Package(
             targets: ["MapplsLMSWrapper"])
     ],
     dependencies: [
-        .package(url: "https://github.com/mappls-api/mappls-api-core-ios-distribution.git", from: "2.0.4")
+        .package(url: "https://github.com/mappls-api/mappls-api-core-ios-distribution.git", from: "2.1.3")
     ],
     targets: [
         .binaryTarget(
             name: "MapplsLMS",
-            url: "https://mmi-api-team.s3.amazonaws.com/mappls-sdk-ios/mappls-lms/MapplsLMS.xcframework-2.0.1.zip",
-            checksum: "abf5a92ab131e6a52425274951ccaa5149e6a3f5931cf9336fcdae7a00c50cce"
+            url: "https://mmi-api-team.s3.amazonaws.com/mappls-sdk-ios/mappls-lms/MapplsLMS.xcframework-2.0.3.zip",
+            checksum: "ff376e583553fc7388430f01be49bd40aeed79bdbbfca2ada994a09b004d6b9d"
         ),
         .target(
             name: "MapplsLMSWrapper",
